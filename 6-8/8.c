3873126f00c5f0f323ffece99f139b02456df5b3
toki2022 <toki2022@163.com>
2022-11-30 22:58:05 +0800

    first commit

--- 6-8/8.c
+++ 6-8/8.c
@@ -16,14 +16,36 @@ int factorsum( int number )
 }
 void PrintPN( int m, int n )
 {
-    for(;m<n;m++)
+    int flag=0;
+    int i;
+    for(i=m;i<n;i++ )
     {
-        factorsum(m)
+        if(factorsum(i)==i)
+        {
+            printf("%d=1",i);
+            for(int j = 2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    printf("+%d",j);
+                }
+            }
+            printf("\n");
+            flag = 1;
+        }
     }
+    if(flag==0)
+    { printf("No perfect number");}
 }
+
 int main()
 {
-    int number=6;
-    printf("%d\n", factorsum(number));
+    int m, n;
+ 
+    scanf("%d %d", &m, &n);
+    if ( factorsum(m) == m ) printf("%d is a perfect number\n", m);
+    if ( factorsum(n) == n ) printf("%d is a perfect number\n", n);
+    PrintPN(m, n);
+ 
     return 0;
 }
