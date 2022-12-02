7e6e329ad52714f87acf1a1c5d62d14ec07e1184
Toki2022 <99173772+Toki2022@users.noreply.github.com>
2022-12-02 10:38:56 +0800

    fib

--- 6-9/9.c
+++ 6-9/9.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int fib(int n)
 {
+    int f;
+    if(n==1|| n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        f=fib(n-1)+fib(n-2);
+    }
+    return f;
+}
+/*{
     int n1 = 1, n2 = 1, n3;
     if (n <= 2)
     {
@@ -13,9 +25,9 @@ int fib(int n)
         n2 = n3;
     }
     return n1;
-}
+}*/
 void PrintFN(int m, int n)
-{
+{   
     int i = 1;
     int flag = 0;  //标记求得的斐波那契数
     int blank = 1; //控制字符前面是否带空格，标记为真，方便输出不带空格的第一个数
